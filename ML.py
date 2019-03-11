f2799b1657946e0ef84b77a1b85d5b0284beb8ec
Porco24 <jzy946929203@qq.com>
2019-03-11 12:23:36 +0800

    look like finish, but predict is negative

--- ML.py
+++ ML.py
@@ -3,17 +3,18 @@ from keras.models import Model
 from keras.models import load_model
 import numpy as np
 import os
+import librosa
 
 
 
-train_data = np.loadtxt("song.csv", delimiter=',')
-train_data = np.swapaxes(train_data,0,1)
-train_data = np.expand_dims(train_data, axis=2)
-#train_data = train_data[:, np.newaxis, np.newaxis, :]
-# train_data = np.swapaxes(train_data,0,3)
-
-lable_data = np.loadtxt("Expert.csv", delimiter=',')
-lable_data = np.expand_dims(lable_data, axis=1)
+# train_data = np.loadtxt("song.csv", delimiter=',')
+# train_data = np.swapaxes(train_data,0,1)
+# train_data = np.expand_dims(train_data, axis=2)
+# #train_data = train_data[:, np.newaxis, np.newaxis, :]
+# # train_data = np.swapaxes(train_data,0,3)
+#
+# lable_data = np.loadtxt("Expert.csv", delimiter=',')
+# lable_data = np.expand_dims(lable_data, axis=1)
 
 
 
@@ -35,19 +36,20 @@ lable_data = np.expand_dims(lable_data, axis=1)
 
 
 
-
-maxlen = train_data.shape[1]
-
-
-
-model_input = Input(shape=(maxlen, 1), name='input')
+model_input = Input(shape=(128, 1), name='input')
 layer = model_input
 layer = Conv1D(filters=40,
-               kernel_size=10,
+               kernel_size=5,
+               padding='valid',
+               activation='relu')(layer)
+layer = MaxPooling1D(pool_size=6,
+                     padding='same')(layer)
+layer = Conv1D(filters=40,
+               kernel_size=5,
                padding='valid',
                activation='relu')(layer)
 layer = BatchNormalization(axis=2)(layer)
-layer = MaxPooling1D(pool_size=40,
+layer = MaxPooling1D(pool_size=6,
                      padding='same')(layer)
 layer = Dropout(0.3)(layer)
 layer = LSTM(40, return_sequences=True,
@@ -112,9 +114,11 @@ def fitMode(path, rate):
         count += 1
         os.chdir("..")
 
-def loadData(path):
-    X = np.zeros((1, 128, 1))
-    Y = np.ndarray((1, 1))
+def loadData(path, rate):
+    X_train = np.zeros((1, 128, 1))
+    Y_train = np.ndarray((1, 1))
+    X_test = np.zeros((1, 128, 1))
+    Y_test = np.ndarray((1, 1))
     os.chdir(path)
     filePath = os.listdir()
     count = 1
@@ -122,8 +126,10 @@ def loadData(path):
     for fileName in os.listdir():
         print(fileName)
         os.chdir(fileName)
-        haveCsv = 0
+        hasY = 0
         for file in os.listdir():
+            if(rate >= float(count / total)):
+                break
             if (os.path.splitext(file)[-1] == ".wav"):
                 songName = os.path.splitext(file)[0]
                 for fileCsv in os.listdir():
@@ -131,30 +137,57 @@ def loadData(path):
                         data = np.loadtxt(fileCsv, delimiter=',')
                         data = np.swapaxes(data,0,1)
                         data = np.expand_dims(data, axis=2)
-                        X = np.concatenate((X, data), axis=0)
+                        if (rate >= float(count / total)):
+                            None
+                            #X_train = np.concatenate((X_train, data), axis=0)
+                        else:
+                            X_test = np.concatenate((X_test, data), axis=0)
                         del data
-                    elif (os.path.splitext(fileCsv)[-1] == ".csv"):
+                    elif (os.path.splitext(fileCsv)[-1] == ".csv" and hasY == 0):
                         data = np.loadtxt(fileCsv, delimiter=',')
                         data = np.expand_dims(data, axis=1)
-                        Y = np.concatenate((Y, data), axis=0)
+                        if (rate >= float(count / total)):
+                            None
+                            #Y_train = np.concatenate((Y_train, data), axis=0)
+                        else:
+                            Y_test = np.concatenate((Y_test, data), axis=0)
                         del data
-                        break
+                        hasY = 1
         print("已完成：", count, "/", total)
         count += 1
         os.chdir("..")
-    return X, Y
+    return X_train, Y_train, X_test, Y_test
 
 
-X, Y = loadData("trainData/unzipFile")
-X_train = X[:200]
-Y_train = Y[:200]
-X_test = X[200:]
-Y_test = Y[200:]
+# X_train, Y_train, X_test, Y_test = loadData("trainData/unzipFile", 0.8)
+# #np.save('X_train.npy', X_train)
+# #np.save('Y_train.npy', Y_train)
+# np.save('X_test.npy', X_test)
+# np.save('Y_test.npy', Y_test)
+#
+# #fit model
+# print('------------Training------------')
+# model.fit(X_train, Y_train)
+#
+# print('------------Test------------')
+# cost = model.evaluate(X_test, Y_test)
+# print('test coast:', cost)
+# print('complete')
 
-#fit model
-print('------------Training------------')
-model.fit(X_train, Y_train)
+# X_train = np.load('X_train.npy')
+# Y_train = np.load('Y_train.npy')
+#
+# model.fit(X_train, Y_train)
+# model.save('first.h5')
 
-print('------------Test------------')
-cost = model.evaluate(X_test, Y_test)
-print('test coast:', cost)
+# print('------------Test------------')
+# cost = model.evaluate(X_test, Y_test)
+# print('test coast:', cost)
+
+data, sample = librosa.load('China-M.wav')
+S = librosa.feature.melspectrogram(y=data, sr=sample)
+S = np.swapaxes(S, 0, 1)
+S = np.expand_dims(S, axis=2)
+myModel = load_model('first.h5')
+predictions = myModel.predict(S)
+print(predictions)
